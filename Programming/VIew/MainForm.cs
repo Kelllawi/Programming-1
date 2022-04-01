8d85e42913771270739e96aed985aa333e1e7ee3
danil <Feedik111>
2022-04-01 12:56:19 +0700

    fix

--- Programming/VIew/MainForm.cs
+++ Programming/VIew/MainForm.cs
@@ -10,7 +10,7 @@ namespace Programming.View
     {
         const int CountElements = 5;
         
-        private Color _errorColor = Color.Pink;жжжж
+        private Color _errorColor = Color.Pink;
         
         private Color _correctColor = Color.White;
         
